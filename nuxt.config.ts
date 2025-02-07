4e43e70adbb39d48c5988877016bba792651e08b
Léopold OHNIMUS <leopold.ohnimus@edu.univ-fcomte.fr>
2025-02-07 16:26:24 +0100

    lien vers l'API en variable

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,7 @@
 import tailwindcss from "@tailwindcss/vite";
 
+console.log(process.env.NODE_ENV);
+
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
@@ -7,6 +9,12 @@ export default defineNuxtConfig({
   modules: [
     '@nuxt/eslint'
   ],
+
+  runtimeConfig: {
+    public: {
+      apiBaseUrl: process.env.NUXT_PUBLIC_API_BASE_URL,
+    },
+  },
 
   css: ['~/assets/css/main.css'],
   vite: {
