dadaf3154e7a8514f72d2f16ecf5ab00a282135d
Léopold OHNIMUS <leopold.ohnimus@edu.univ-fcomte.fr>
2025-02-10 22:36:05 +0100

    lint fix

--- plugins/useAuth.ts
+++ plugins/useAuth.ts
@@ -2,7 +2,7 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
-export default defineNuxtPlugin((nuxtApp) => {
+export default defineNuxtPlugin(() => {
   const config = useRuntimeConfig().public;
 
   const firebaseConfig = {
