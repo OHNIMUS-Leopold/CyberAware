4443c10babda344064fe4bc1dc2d1fc2a2034888
Léopold OHNIMUS <leopold.ohnimus@edu.univ-fcomte.fr>
2025-02-10 18:56:44 +0100

    simplification ref togglemenu

--- components/MyHeader.vue
+++ components/MyHeader.vue
@@ -1,18 +1,10 @@
 <script setup lang="ts">
-const menuToggle = ref();
-const mobileMenu = ref();
+const menuToggle = ref(false);
 
-onMounted(() => {
-    menuToggle.value = document.getElementById('menu-toggle');
-    mobileMenu.value = document.getElementById('mobile-menu');
-    menuToggle.value.addEventListener('click', () => {
-        mobileMenu.value.classList.toggle('hidden');
-        mobileMenu.value.classList.toggle('translate-y-0');
-        mobileMenu.value.classList.toggle('opacity-100');
-    });
-});
+const toggleMenu = () => {
+    menuToggle.value = !menuToggle.value;
+};
 </script>
-
 
 <template>
     <header class="bg-white text-black border-b-2 border-primary shadow-md fixed top-0 left-0 w-full z-10">
@@ -31,17 +23,21 @@ onMounted(() => {
             </nav>
 
             <!-- Bouton Mobile -->
-            <button id="menu-toggle" class="md:hidden focus:outline-none">
-                <svg id="menu-icon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                    stroke="currentColor" class="w-10 h-10 text-black">
+            <button @click="toggleMenu" class="md:hidden focus:outline-none transition-transform">
+                <svg v-if="!menuToggle" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                    stroke="currentColor" class="w-10 h-10 text-black transform transition-transform">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16m-7 6h7" />
+                </svg>
+                <svg v-else xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                    stroke="currentColor" class="w-10 h-10 text-black transform transition-transform">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
                 </svg>
             </button>
         </div>
         
         <!-- Navigation Mobile -->
-        <nav id="mobile-menu"
-            class="hidden md:hidden flex-col bg-white border-b-2 border-t-2 border-t-gray-200 border-primary text-black p-6 space-y-4 absolute inset-x-0 top-[4.5rem] shadow-lg rounded-b-lg ">
+        <nav :class="{'hidden': !menuToggle, 'flex': menuToggle, 'translate-y-0': menuToggle, 'opacity-100': menuToggle}"
+            class="md:hidden flex-col bg-white border-b-2 border-t-2 border-t-gray-200 border-primary text-black p-6 space-y-4 absolute inset-x-0 top-[4.5rem] shadow-lg rounded-b-lg transition-all duration-300">
             <NuxtLink to="/" class="block py-2 px-4 hover:text-primary">Accueil</NuxtLink>
             <hr class="text-gray-200">
             <NuxtLink to="/" class="block py-2 px-4 hover:text-primary ">S'informer</NuxtLink>
