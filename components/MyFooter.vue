fca584be83e8b5ac9b70caac1d900bb1236313b6
Léopold OHNIMUS <leopold.ohnimus@edu.univ-fcomte.fr>
2025-02-11 16:37:05 +0100

    modif nav

--- components/MyFooter.vue
+++ components/MyFooter.vue
@@ -1,3 +1,26 @@
+<script setup lang="ts">
+import { onAuthStateChanged } from "firebase/auth";
+import { doc, getDoc } from "firebase/firestore";
+
+const { $firebase } = useNuxtApp();
+const auth = $firebase.auth;
+const db = $firebase.db;
+
+const userName = ref<string | null>(null);
+
+onAuthStateChanged(auth, async (user) => {
+    if (user) {
+        const userDoc = await getDoc(doc(db, "users", user.uid));
+        if (userDoc.exists()) {
+            userName.value = userDoc.data().pseudo;
+        }
+    } else {
+        userName.value = null;
+    }
+});
+</script>
+
+
 <template>
     <footer class="bg-black text-white py-6 w-full absolute transform -translate-x-1/2 left-1/2">
         <div class="mx-auto px-4 md:px-8">
@@ -5,8 +28,11 @@
             <nav class="flex flex-wrap justify-center gap-6 text-white">
                 <RouterLink to="/" class="hover:text-primary">Accueil</RouterLink>
                 <RouterLink to="/" class="hover:text-primary">S'informer</RouterLink>
-                <RouterLink to="/" class="hover:text-primary">Outils</RouterLink>
                 <RouterLink to="/" class="hover:text-primary">Signaler</RouterLink>
+                <div class="contents">
+                    <NuxtLink v-if="userName" to="/auth" class="hover:text-primary">Se déconnecter</NuxtLink>
+                    <NuxtLink v-else to="/auth" class="hover:text-primary">S'authentifier</NuxtLink>
+                </div>
             </nav>
 
             <div class="border-t border-gray-700 my-5"/>
