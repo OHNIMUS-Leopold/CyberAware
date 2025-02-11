918d0d9e72d8972202f499c4fd180880c8bb449f
Léopold OHNIMUS <leopold.ohnimus@edu.univ-fcomte.fr>
2025-02-11 18:53:39 +0100

    delete old components

--- components/OutlineButton.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <button class="text-primary hover:text-secondary hover:border-secondary border-2 border-primary duration-500 hover:cursor-pointer py-3 px-10 rounded-xl">
-        <slot />
-    </button>
-</template>
