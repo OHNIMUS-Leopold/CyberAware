8646e466275dd98e70e7521fe2afd1bd7f97e8bc
Léopold OHNIMUS <leopold.ohnimus@edu.univ-fcomte.fr>
2025-02-11 09:59:10 +0100

    correction tailles et marges

--- components/MyFooter.vue
+++ components/MyFooter.vue
@@ -1,8 +1,8 @@
 <template>
     <footer class="bg-black text-white py-6 w-full absolute transform -translate-x-1/2 left-1/2">
-        <div class="container mx-auto px-6 md:px-12">
+        <div class="mx-auto px-4 md:px-8">
 
-            <nav class="flex flex-wrap justify-center gap-6 text-white text-base">
+            <nav class="flex flex-wrap justify-center gap-6 text-white">
                 <RouterLink to="/" class="hover:text-primary">Accueil</RouterLink>
                 <RouterLink to="/" class="hover:text-primary">S'informer</RouterLink>
                 <RouterLink to="/" class="hover:text-primary">Outils</RouterLink>
